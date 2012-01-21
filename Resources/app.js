e9877af0d5cbcc369af08382227ef84b02d98c55
Joe Wheaton <joseph.wheaton@gmail.com>
2012-01-20 17:06:54 -0800

    doesn't work yet, but some structure changes were added.

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,21 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+require('lib/require').monkeypatch(this);
+var globals = {
+  osname: Ti.Platform.osname,
+  debugMode: false
+};
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+var used = [Ti.UI.createButton, Ti.UI.createWindow, Ti.UI.iPhone.createNavigationGroup, Ti.UI.createTableView];
 
+var testflight = require('com.0x82.testflight');
+testflight.takeOff('d6b198db921f8ef931965685eeee400c_MzA5NTgyMDExLTA5LTIzIDE0OjI2OjMxLjQ2NjI3Nw');
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+(function() {
+  var WindowObject;
+  if (globals.osname === 'iphone' || globals.osname === 'ipad') {
+    WindowObject = require('/ui/iphon/iPhoneWindow');
+  }
+  else {
+    WindowObject = require('/ui/android/AndroidWindow');
+  }
+  new WindowObject().open();
+})();
