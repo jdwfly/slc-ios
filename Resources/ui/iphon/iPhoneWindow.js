f403b3627dbbdc4fcc24ee10277526a4774f65e1
Joe Wheaton <joseph.wheaton@gmail.com>
2012-04-03 10:11:59 -0700

    Removed sliding menu functionality.

--- Resources/ui/iphon/iPhoneWindow.js
+++ Resources/ui/iphon/iPhoneWindow.js
@@ -1,27 +1,14 @@
 exports.iPhoneWindow = function() {
-  var leftNavButton = Ti.UI.createButton({
-    title: 'Menu'
-  });
-  leftNavButton.addEventListener('click', function() {
-    if (navGroup.slide) {
-      navGroup.animate({left: 0, duration: 500});
-      navGroup.slide = false;
-    } else {
-      navGroup.animate({left: 250, duration: 700});
-      navGroup.slide = true;
-    }
-  });
   var instance = Ti.UI.createWindow({
     backgroundColor: '#ffffff'
   });
   var mainWindow = Ti.UI.createWindow({
     title: 'Schedule',
     backgroundColor: '#ffffff',
-    leftNavButton: leftNavButton,
     barColor: '#64770d'
   });
   var viewport = Ti.UI.createScrollView({});
-  toolbar = require('ui/common/scheduleToolbar');
+  toolbar = require('ui/common/scheduleToolbar').scheduleToolbar;
   scheduleToolbar = new toolbar({
     top: 0,
     height: 44,
@@ -33,26 +20,11 @@ exports.iPhoneWindow = function() {
   });
   mainWindow.add(scheduleToolbar);
   
-  var data = [];
-  db().each(function(e) {
-    data.push({title: e.name});
-  });
-  var tableView = Ti.UI.createTableView({
-    top: 44,
-    data: data
-  });
-  Ti.API.info(data);
-  //viewport.add(tableView);
-  mainWindow.add(tableView);
-  var sideMenu = require('ui/common/sideMenu');
-  var mainMenu = new sideMenu();
-  
   var navGroup = Ti.UI.iPhone.createNavigationGroup({
     window: mainWindow,
     width: '100%',
     slide: false
   });
-  instance.add(mainMenu);
   instance.add(navGroup);
   
   return instance;
