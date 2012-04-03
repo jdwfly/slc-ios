d99d35f479e39cec57fb59f101cfecbbd779187b
Joe Wheaton <joseph.wheaton@gmail.com>
2012-04-03 10:12:40 -0700

    lots of changes here.

--- Resources/app.js
+++ Resources/app.js
@@ -1,24 +1,35 @@
-require('lib/require').monkeypatch(this);
-Ti.taffy = require('lib/taffy').taffyDb;
-
 // GLOBAL VARS
-var globals = {
-  osname: Ti.Platform.osname,
-  debugMode: true,
-  firstRun: Ti.App.Properties.getBool('firstRun', true),
-  db: require('lib/db')
-};
+var globals = require('lib/globals');
+var HTTPClientWithCache = require('lib/HTTPClientWithCache').HTTPClientWithCache;
+Ti.API.info(globals.osname);
+var MainTabView;
+if (globals.osname === 'iphone' || globals.osname === 'android') {
+  MainTabView = require('/ui/common/mainTabView').mainTabView;
+}
+else {
+  MainTabView = require('/ui/ipad/ipadMainWindow');
+}
+new MainTabView().open({transition:Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT});
 
-// Initialize the taffy database with json
-db = globals.db.init();
-
-(function() {
-  var WindowObject;
-  if (globals.osname === 'iphone' || globals.osname === 'ipad') {
-    WindowObject = require('/ui/iphon/iPhoneWindow');
+// Global Event Listeners
+// TODO Make this actually use the _callback
+Ti.App.addEventListener('events.update', function(_callback){
+  if (Ti.Network.online) {
+    var events_xhr = new HTTPClientWithCache({
+      baseUrl: globals.baseUrl,
+      retryCount: 2,
+      cacheSeconds: 3600,
+      onload: function(response) {
+        Ti.API.info("Response Data: "+ response.responseText);
+        Ti.API.info("Is this cached data?: " + response.cached);
+      }
+    });
+    events_xhr.post({url: globals.eventsUrl});
+  } else {
+    var dialog = Ti.UI.createAlertDialog({
+      message: 'You must be online to refresh the application data.',
+      ok: 'Okay',
+      title: 'Oh noes!'
+    }).show();
   }
-  else {
-    WindowObject = require('/ui/android/AndroidWindow');
-  }
-  new WindowObject().open({transition:Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT});
-})();
+});
