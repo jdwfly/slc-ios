ee5b8c6dc6413138a2834c6e1aacf141cf2ba4ee
Joe Wheaton <joseph.wheaton@gmail.com>
2012-04-25 09:26:03 -0700

    basic structure setup for workshop window

--- Resources/ui/common/workshopWindow.js
+++ Resources/ui/common/workshopWindow.js
@@ -5,6 +5,48 @@ exports.workshopWindow = function(opts) {
     backgroundColor: '#eeeeee',
     barColor: '#3b587b'
   });
+  instance.orientationModes = [Ti.UI.PORTRAIT];
+  
+  // Android Specific Code
+  if (globals.osname === 'android') {
+    instance.backgroundColor = "#111111";
+    instance.activity.onCreateOptionsMenu = function(e) {
+      var menu = e.menu;
+      var menuItem = menu.add({title:"Refresh"});
+      menuItem.addEventListener("click", function(e) {
+        Ti.App.fireEvent('events.update');
+      });
+    };
+  }
+  
+  // iPhone Specific Code
+  if (globals.osname === 'iphone') {
+    var refresh = Ti.UI.createButton({
+      systemButton:Ti.UI.iPhone.SystemButton.REFRESH
+    });
+    refresh.addEventListener('click', function(e) {
+      Ti.App.fireEvent('events.update');
+    });
+    instance.rightNavButton = refresh;
+  }
+  
+  var tableData = [], workshopTableView, row;
+  
+  if (opts.nodes.rowCount != 0) {
+    while (opts.nodes.isValidRow()) {
+      row = {title: opts.nodes.fieldByName('title')};
+      tableData.push(row);
+      opts.nodes.next();
+    }
+  } else {
+    tableData = [{title: 'No results'}];
+  }
+  workshopTableView = Ti.UI.createTableView({
+    data: tableData,
+    separatorColor: '#eeeeee',
+    backgroundColor: '#eeeeee'
+  });
+  instance.add(workshopTableView);
   
   return instance;
 };
