95ebb69dc64c2463a9474dcaa5a76a3e59516fca
Joe Wheaton <joseph.wheaton@gmail.com>
2012-05-08 16:50:27 -0700

    continued work on photoDetail window.

--- Resources/ui/common/photosDetailWindow.js
+++ Resources/ui/common/photosDetailWindow.js
@@ -1,6 +1,33 @@
-exports.photosDetailWindow = function() {
+var globals = require('lib/globals');
+
+exports.photosDetailWindow = function(opts) {
   var instance = Ti.UI.createWindow({
-    
+    backgroundColor: '#eeeeee',
+    barColor: '#3b587b',
+    fullscreen: true
+  });
+  instance.orientationModes = [Ti.UI.PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT];
+  
+  Ti.API.info("Image = " + opts.image);
+  
+  var image = Ti.UI.createImageView({
+    image: "http://www.lancasterbaptist.org/slc/" + globals.html_decode(opts.image),
+    height: Ti.UI.FILL,
+    width: Ti.UI.FILL
+  });
+  
+  var scroll = Ti.UI.createScrollView({
+    maxZoomScale: 10,
+    minZoomScale: 1.0,
+    backgroundColor: 'transparent'
+  });
+  
+  scroll.add(image)
+  
+  instance.add(scroll);
+  
+  instance.addEventListener('open', function(e) {
+    this.setNavBarHidden(true);
   });
   
   
