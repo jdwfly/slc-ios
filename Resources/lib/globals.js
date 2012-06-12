44dd7d1ba2ce3787cc1703ce1821bc242696ef6f
Joe Wheaton <joseph.wheaton@gmail.com>
2012-06-12 10:15:44 -0700

    added two phpjs functions for arrays

--- Resources/lib/globals.js
+++ Resources/lib/globals.js
@@ -715,3 +715,80 @@ exports.prettyDate = function(time) {
   day_diff < 7 && day_diff + " days ago" ||
   day_diff < 31 && Math.ceil( day_diff / 7 ) + " week" + ((Math.ceil( day_diff / 7 )) == 1 ? "" : "s") + " ago";
 }
+
+exports.in_array = function (needle, haystack, argStrict) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: vlado houba
+    // +   input by: Billy
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: in_array('van', ['Kevin', 'van', 'Zonneveld']);
+    // *     returns 1: true
+    // *     example 2: in_array('vlado', {0: 'Kevin', vlado: 'van', 1: 'Zonneveld'});
+    // *     returns 2: false
+    // *     example 3: in_array(1, ['1', '2', '3']);
+    // *     returns 3: true
+    // *     example 3: in_array(1, ['1', '2', '3'], false);
+    // *     returns 3: true
+    // *     example 4: in_array(1, ['1', '2', '3'], true);
+    // *     returns 4: false
+    var key = '',
+        strict = !! argStrict;
+    if (strict) {
+        for (key in haystack) {
+            if (haystack[key] === needle) {
+                return true;
+            }
+        }
+    } else {
+        for (key in haystack) {
+            if (haystack[key] == needle) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+exports.array_search = function(needle, haystack, argStrict) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: array_search('zonneveld', {firstname: 'kevin', middle: 'van', surname: 'zonneveld'});
+    // *     returns 1: 'surname'
+    // *     example 2: ini_set('phpjs.return_phpjs_arrays', 'on');
+    // *     example 2: var ordered_arr = array({3:'value'}, {2:'value'}, {'a':'value'}, {'b':'value'});
+    // *     example 2: var key = array_search(/val/g, ordered_arr); // or var key = ordered_arr.search(/val/g);
+    // *     returns 2: '3'
+
+    var strict = !!argStrict,
+        key = '';
+    
+    if (haystack && typeof haystack === 'object' && haystack.change_key_case) { // Duck-type check for our own array()-created PHPJS_Array
+        return haystack.search(needle, argStrict);
+    }
+    if (typeof needle === 'object' && needle.exec) { // Duck-type for RegExp
+        if (!strict) { // Let's consider case sensitive searches as strict
+            var flags = 'i' + (needle.global ? 'g' : '') +
+                        (needle.multiline ? 'm' : '') +
+                        (needle.sticky ? 'y' : ''); // sticky is FF only
+            needle = new RegExp(needle.source, flags);
+        }
+        for (key in haystack) {
+            if (needle.test(haystack[key])) {
+                return key;
+            }
+        }
+        return false;
+    }
+
+    for (key in haystack) {
+        if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
+            return key;
+        }
+    }
+
+    return false;
+}
+
