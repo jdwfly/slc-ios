f49aeae94de0da7b32182d9bb4d37895c91489cf
Joe Wheaton <joseph.wheaton@gmail.com>
2014-05-10 17:12:18 -0700

    Switch to the livestream.com player.

--- Resources/ui/iphon/livePlayerWindow.js
+++ Resources/ui/iphon/livePlayerWindow.js
@@ -1,7 +1,7 @@
 exports.livePlayerWindow = function() {
   var instance = Ti.UI.createWindow({});
   instance.orientationModes = [Ti.UI.PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT];
-  
+  /**
   var player = Ti.Media.createVideoPlayer({
     url: "http://lancaster-apple-live.adaptive.level3.net/apple/cwie-lbc/lbc/lbc_iphone.m3u8",
     mediaControlStyle: Ti.Media.VIDEO_CONTROL_DEFAULT,
@@ -19,6 +19,12 @@ exports.livePlayerWindow = function() {
     player.stop();
     Ti.UI.orientation = Ti.UI.PORTRAIT;
   });
+  */
+  var webview = Ti.UI.createWebView({
+    url: '/ui/static/livestream.html',
+    scalesPageToFit: true
+  });
+  instance.add(webview);
   
   return instance;
 };
