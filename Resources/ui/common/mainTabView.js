288b00fca94dc8280eb3dd84c9b3de3f2673a724
Joe Wheaton <joseph.wheaton@gmail.com>
2014-05-14 17:03:42 -0700

    Setup tabs to work via a config file, no more commenting.

--- Resources/ui/common/mainTabView.js
+++ Resources/ui/common/mainTabView.js
@@ -2,63 +2,74 @@ var globals = require('lib/globals');
 exports.mainTabView = function() {
   var instance = Ti.UI.createTabGroup();
   
-  /*
-  // Not Used after conference
-  var scheduleWindow = require('ui/common/scheduleWindow').scheduleWindow;
-  var scheduleTab = Ti.UI.createTab({  
-    icon: 'data/11-clock.png',
-    title: 'Schedule',
-    window: new scheduleWindow()
-  });
-  instance.addTab(scheduleTab);
-  */
-  var sessionWindow = require('ui/common/sessionWindow').window;
-  var sessionTab = Ti.UI.createTab({
-    icon: 'data/120-headphones.png',
-    title: 'Sessions',
-    window: new sessionWindow()
-  });
-  instance.addTab(sessionTab);
+  // Grab the config.json file
+  var configFile = Ti.Filesystem.getFile("config.json");
+  var preParseData = (configFile.read().text);
+  var config = JSON.parse(preParseData);
 
-  var mapsWindow = require('ui/common/mapsWindow').mapsWindow;
-  var mapsTab = Ti.UI.createTab({
-    icon: 'data/103-map.png',
-    title: 'Maps',
-    window: new mapsWindow()
-  });
-  
-  var newsWindow = require('ui/common/newsWindow').newsWindow;
-  var newsTab = Ti.UI.createTab({
-    icon: 'data/45-movie-1.png',
-    title: 'Videos',
-    window: new newsWindow()
-  });
-  
-  var speakersWindow = require('ui/common/speakersWindow').speakersWindow;
-  var speakersTab = Ti.UI.createTab({
-    icon: 'data/112-group.png',
-    title: 'Speakers',
-    window: new speakersWindow()
-  });
-  
-  var liveWindow = require('ui/iphon/liveWindow').liveWindow;
-  var liveTab = Ti.UI.createTab({
-    icon: 'data/69-display.png',
-    title: 'Live',
-    window: new liveWindow()
-  });
-  
-  instance.addTab(mapsTab);
-  instance.addTab(newsTab);
-  instance.addTab(speakersTab);
-  if (globals.osname === 'iphone' || globals.osname === 'ipad') {
-    instance.addTab(liveTab);
-  } else if (globals.osname === 'android' && Ti.Platform.Android.API_LEVEL >= 14) {
+  if (config.tabs.schedule === true){
+    var scheduleWindow = require('ui/common/scheduleWindow').scheduleWindow;
+    var scheduleTab = Ti.UI.createTab({
+      icon: 'data/11-clock.png',
+      title: 'Schedule',
+      window: new scheduleWindow()
+    });
+    instance.addTab(scheduleTab);
+  }
+
+  if (config.tabs.sessions === true) {
+    var sessionWindow = require('ui/common/sessionWindow').window;
+    var sessionTab = Ti.UI.createTab({
+      icon: 'data/120-headphones.png',
+      title: 'Sessions',
+      window: new sessionWindow()
+    });
+    instance.addTab(sessionTab);
+  }
+
+  if (config.tabs.maps === true) {
+    var mapsWindow = require('ui/common/mapsWindow').mapsWindow;
+    var mapsTab = Ti.UI.createTab({
+      icon: 'data/103-map.png',
+      title: 'Maps',
+      window: new mapsWindow()
+    });
+    instance.addTab(mapsTab);
+  }
+
+  if (config.tabs.news === true) {
+    var newsWindow = require('ui/common/newsWindow').newsWindow;
+    var newsTab = Ti.UI.createTab({
+      icon: 'data/45-movie-1.png',
+      title: 'Videos',
+      window: new newsWindow()
+    });
+    instance.addTab(newsTab);
+  }
+
+  if (config.tabs.speakers === true) {
+    var speakersWindow = require('ui/common/speakersWindow').speakersWindow;
+    var speakersTab = Ti.UI.createTab({
+      icon: 'data/112-group.png',
+      title: 'Speakers',
+      window: new speakersWindow()
+    });
+    instance.addTab(speakersTab);
+  }
+
+  if (config.tabs.live === true) {
+    var liveWindow = require('ui/iphon/liveWindow').liveWindow;
+    var liveTab = Ti.UI.createTab({
+      icon: 'data/69-display.png',
+      title: 'Live',
+      window: new liveWindow()
+    });
     instance.addTab(liveTab);
   }
-  
+
   // Workaround for not knowing the current tab
   // http://developer.appcelerator.com/question/98501/titaniumuicurrenttab-is-null
+  /*
   instance.addEventListener('focus', function(e) {
     instance._activeTab = e.tab;
     instance._activeTabIndex = e.index;
@@ -68,6 +79,6 @@ exports.mainTabView = function() {
     Ti.API._activeTab = instance._activeTab;
     //Ti.API.info(Ti.API._activeTab.title);
   });
-  
+  */
   return instance;
 };
